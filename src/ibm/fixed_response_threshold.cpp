ccd5192a697ac6dbbb60bdb9de2ced9c4d986538
Tucker <ngt206@carson.cm.cluster>
2018-04-27 16:16:15 +0100

    annoying little copy-paste error throwing an assert

--- src/ibm/fixed_response_threshold.cpp
+++ src/ibm/fixed_response_threshold.cpp
@@ -335,7 +335,6 @@ void Init(Population & Pop, Params & Par)
         assert(Pop[i].workfor.empty());
 
         // erase any existing fitness data
-        if (!Pop[i].workfor.empty())
         if (!Pop[i].last_half_acts.empty()) 
         {
             Pop[i].last_half_acts.erase(
